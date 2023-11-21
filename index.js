9f438552277be7649c06e031502e80c9f6348d21
provadorvirtual <146850400+provadorvirtual@users.noreply.github.com>
2023-11-21 09:51:02 -0300

    test

--- index.js
+++ index.js
@@ -23,3 +23,20 @@ bolhasong.addEventListener("click", function(){
     const audio2 = document.querySelector(".bolha")
     audio2.play()
 });
+
+function SearchFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    ul = document.querySelector("rp");
+    li = ul.querySelector("nameprod");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("p")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
